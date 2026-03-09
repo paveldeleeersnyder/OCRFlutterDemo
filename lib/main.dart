3d4a5b5edf0aba2dc9f88a2e1bcc7570695b8753
paveldel <pavel.deleersnyder@student.howest.be>
2026-03-09 09:04:17 +0100

    DEMO READY

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import './document_scanner.dart';
+import './document_scanner.dart'; 
 
 void main() async {
   await dotenv.load(fileName: ".env");
